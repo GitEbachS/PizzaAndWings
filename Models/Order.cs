9b176a6a44540be24ae003e48b77f47c01fba7ac
Shari Ebach <ebachshari@yahoo.com>
2024-04-06 12:10:32 -0500

    setup

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,6 +6,7 @@ public class Order
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
     public string Email { get; set; }
     public string Phone { get; set; }
     public bool Status { get; set; }

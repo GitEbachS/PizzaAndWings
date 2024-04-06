fbce7eec2809dd549fbf5074bebece26ebeb1596
Shari Ebach <ebachshari@yahoo.com>
2024-04-06 12:53:25 -0500

    updated seeds

--- Controllers/OrdersApi.cs
+++ Controllers/OrdersApi.cs
@@ -6,39 +6,40 @@ namespace PizzaAndWings.Controllers
     {
         public static void Map(WebApplication app)
         {
-                app.MapGet("/orders", (PizzaAndWingsDbContext db) =>
-                {
-                    var orders = db.Orders
-                        .Include(o => o.Items)
-                        .Include(o => o.PaymentType)
-                        .Include(o => o.OrderType)
-                        .Where(o => o.DateClosed <= DateTime.UtcNow)
-                        .OrderByDescending(o => o.DateClosed)
-                        .Select(o => new
+            app.MapGet("/orders", (PizzaAndWingsDbContext db) =>
+            {
+                var orders = db.Orders
+                    .Include(o => o.Items)
+                    .Include(o => o.PaymentType)
+                    .Include(o => o.OrderType)
+                    .Where(o => o.DateClosed <= DateTime.UtcNow)
+                    .OrderByDescending(o => o.DateClosed)
+                    .Select(o => new
+                    {
+                        o.Id,
+                        FullName = $"{o.FirstName} {o.LastName}",
+                        o.Email,
+                        DateClosed = o.DateClosed.ToString("MM/dd/yyyy"),
+                        o.Phone,
+                        o.OrderTypeId,
+                        OrderType = o.OrderType.Type,
+                        o.Status,
+                        ItemTotal = o.ItemTotal, // Use ItemTotal property instead of Total
+                        o.Tip,
+                        o.PaymentTypeId,
+                        PaymentType = o.PaymentType.Type,
+                        TotalWithTip = o.TotalWithTip, // Use TotalWithTip property instead of Total
+                        Items = o.Items.Select(i => new
                         {
-                            o.Id,
-                            FullName = $"{o.FirstName} {o.LastName}",
-                            o.Email,
-                            DateClosed = o.DateClosed.ToString("MM/dd/yyyy"),
-                            o.Phone,
-                            o.OrderTypeId,
-                            OrderType = o.OrderType.Type,
-                            o.Status,
-                            ItemTotal = o.ItemTotal, // Use ItemTotal property instead of Total
-                            o.Tip,
-                            o.PaymentTypeId,
-                            PaymentType = o.PaymentType.Type,
-                            TotalWithTip = o.TotalWithTip, // Use TotalWithTip property instead of Total
-                            Items = o.Items.Select(i => new
-                            {
-                                i.Id,
-                                i.OrderItem,
-                                i.OrderPrice
-                            })
+                            i.Id,
+                            i.OrderItem,
+                            i.OrderPrice
                         })
-                        .ToList();
+                    })
+                    .ToList();
 
-                    return Results.Ok(orders);
-                });
-            }
+                return Results.Ok(orders);
+            });
+        }
+    }
 }
---
+++ Migrations/20240406175218_UpdateSeededData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaAndWings.Migrations
+{
+    public partial class UpdateSeededData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateClosed",
+                value: new DateTime(2024, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateClosed",
+                value: new DateTime(2024, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateClosed",
+                value: new DateTime(2024, 6, 11, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateClosed",
+                value: new DateTime(2024, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
